1334cdb88f1aaa9b85ece8b994a04b9829ba1001
Eric Frey <ericlfrey@gmail.com>
2023-04-06 16:58:49 -0500

    Worked through the first chapter and then created a repo. Why? I dunno, that's just how they taught us in the last project.

---
+++ views/orders_requests.py
@@ -0,0 +1,15 @@
+ORDERS = [
+    {
+        "id": 1,
+        "metalId": 3,
+        "sizeId": 2,
+        "styleId": 3,
+        "jewelryId": 2,
+        "timestamp": 1614659931693
+    }
+]
+
+
+def get_all_orders():
+    """Handles Server request for all ORDERS"""
+    return ORDERS
